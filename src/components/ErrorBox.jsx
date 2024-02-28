3732710a5497d593763f36ac685b45c2a27cb928
isaiabarzoi <isaiabarzoi@gmail.com>
2024-02-28 15:58:41 +0100

    sending get request and handling error

---
+++ src/components/ErrorBox.jsx
@@ -0,0 +1,15 @@
+export default function ErrorBox({ title, message, onConfirm }) {
+  return (
+    <div className="error">
+      <h2>{title}</h2>
+      <p>{message}</p>
+      {onConfirm && (
+        <div id="confirmation-action">
+          <button onClick={onConfirm} className="button">
+            Okay
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
